e257e9e7dc725417dd057c3b0332f1d4531dc880
Jeff Buttars <jeffbuttars@gmail.com>
2013-12-05 17:31:50 -0700

    Query supports listing linstalled pkgs.

--- pkgs/cmds/query.py
+++ pkgs/cmds/query.py
@@ -1,4 +1,11 @@
+import logging
+logger = logging.getLogger('pkgs')
+
+import os
+
 from cmds.base import BaseCmd
+from conf import settings
+from lib import nice_pkg_name
 
 
 class Cmd(BaseCmd):
@@ -13,15 +20,52 @@ class Cmd(BaseCmd):
         :rtype:
         """
 
-        # self._cmd_parser.add_argument(
-        #     's',
-        #     type=str,
-        #     default=None,
-        #     # nargs="?",
-        #     nargs=1,
-        #     help=(""),
-        # )
+        self._cmd_parser.add_argument(
+            'query',
+            type=str,
+            default=None,
+            nargs="*",
+            # nargs=1,
+            help=("Get information about installed 'pkgs'"),
+        )
 
         return super(Cmd, self).build()
     #build()
+
+    def exec(self, args):
+        """todo: Docstring for exec
+
+        :param args: arg description
+        :type args: type description
+        :return:
+        :rtype:
+        """
+
+        logger.debug("query %s", args.query)
+
+        pl = self.list_pkgs()
+        if not pl:
+            print("No packages installed, use 'pkgs install'")    
+
+        for p in pl:
+            print(p)
+        # end for p in self.lis
+    #exec()
+
+    def list_pkgs(self):
+        """todo: Docstring for self.list_pkgs
+        :return:
+        :rtype:
+        """
+
+        dlist = os.listdir(settings.pkgs_destdir)
+        res = []
+        for d in dlist:
+            dp = os.path.join(settings.pkgs_destdir, d)
+            if os.path.isdir(dp):
+                res.append(nice_pkg_name(d))
+        # end for d in dlist
+
+        return res
+    #list_pkgs()
 # Cmd
