f2d0e6822a7f5e6c149aae39cf0a6dff4e4568f5
Jeff Buttars <jeffbuttars@gmail.com>
2013-12-05 17:32:01 -0700

    Remove works

--- pkgs/cmds/remove.py
+++ pkgs/cmds/remove.py
@@ -1,11 +1,17 @@
+import logging
+logger = logging.getLogger('pkgs')
+
+import shutil
+
+from lib import pkg_name_to_path, did_u_mean
 from cmds.base import BaseCmd
 
 
 class Cmd(BaseCmd):
     """Docstring for Search """
 
-    name = 'update'
-    help_text = ("update installed pkgs")
+    name = 'remove'
+    help_text = ("remove installed pkg(s)")
 
     def build(self):
         """todo: Docstring for build
@@ -13,15 +19,60 @@ class Cmd(BaseCmd):
         :rtype:
         """
 
-        # self._cmd_parser.add_argument(
-        #     's',
-        #     type=str,
-        #     default=None,
-        #     # nargs="?",
-        #     nargs=1,
-        #     help=(""),
-        # )
+        self._cmd_parser.add_argument(
+            'remove',
+            type=str,
+            default=None,
+            nargs="+",
+            help=("Remove an installed pkg"),
+        )
 
         return super(Cmd, self).build()
     #build()
+
+    def exec(self, args):
+        """todo: Docstring for exec
+
+        :param args: arg description
+        :type args: type description
+        :return:
+        :rtype:
+        """
+
+        logger.debug("remove %s", args.remove)
+
+        for pkg in args.remove:
+            self.remove_repo(pkg)
+        # end for repo in args.remove
+    #exec()
+
+    def remove_repo(self, repo):
+        """todo: Docstring for remove_repo
+
+        :param repo: arg description
+        :type repo: type description
+        :return:
+        :rtype:
+        """
+        logger.debug("%s", repo)
+
+        pkg = pkg_name_to_path(repo)
+
+        logger.debug("pkg path %s", pkg)
+        if not pkg:
+            print(
+                "unable to find pkg '%s'. %s" % (repo, did_u_mean(repo))
+            )
+
+        # Does the repo have any uncommitted changes?
+        # Is the repo out of sync(needs a push?)
+
+        # Are you sure?
+        resp = input("Are you sure you want to remove the '%s' pkg? [y|N] " % repo).lower()
+
+        if resp == 'y' or resp == 'yes':
+            print('removing %s...' % repo)
+            shutil.rmtree(pkg)
+
+    #remove_repo()
 # Cmd
