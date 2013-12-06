2d5c502422d648fa549eb4539507963231f72be7
Jeff Buttars <jeffbuttars@gmail.com>
2013-12-05 17:51:59 -0700

    Started work on the update command.

--- pkgs/cmds/update.py
+++ pkgs/cmds/update.py
@@ -1,11 +1,15 @@
+import logging
+logger = logging.getLogger('pkgs')
+
 from cmds.base import BaseCmd
+from lib import installed_list()
 
 
 class Cmd(BaseCmd):
     """Docstring for Search """
 
     name = 'update'
-    help_text = ("update pkgs")
+    help_text = ("update all or some pkgs")
 
     def build(self):
         """todo: Docstring for build
@@ -13,15 +17,46 @@ class Cmd(BaseCmd):
         :rtype:
         """
 
-        # self._cmd_parser.add_argument(
-        #     's',
-        #     type=str,
-        #     default=None,
-        #     # nargs="?",
-        #     nargs=1,
-        #     help=(""),
-        # )
+        self._cmd_parser.add_argument(
+            'update',
+            type=str,
+            default=None,
+            nargs="*",
+            help=(""),
+        )
 
         return super(Cmd, self).build()
     #build()
+
+    def exec(self, args):
+        """todo: Docstring for exec
+
+        :param args: arg description
+        :type args: type description
+        :return:
+        :rtype:
+        """
+
+        logger.debug("update %s", args.update)
+        self.update(args.update) 
+    #exec()
+
+    def update(self, repos):
+        """
+
+        :param repos: arg description
+        :type repos: type description
+        :return:
+        :rtype:
+        """
+
+        pass
+        rlist = []
+        if not repos:
+            # Update them all!
+            rlist = installed_list()
+        else:
+            # make sure the supplied repos are valid, if they are, update them!
+            pass
+    #update()
 # Cmd
