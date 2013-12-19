f2fd7957633ffbbe1c158af4096e6cf9b6aff297
Jeff Buttars <jeffbuttars@gmail.com>
2013-12-18 18:24:46 -0700

    bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='upkg',
-      version='0.1.2',
+      version='0.1.3',
       description="Package Yourself",
       url='https://github.com/jeffbuttars/upkg',
       long_description=(open('README.md').read()),
