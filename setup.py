336166d8b5dcb8a7d6029e9f80c7601c8189f7af
Jeff Buttars <jeffbuttars@gmail.com>
2014-03-17 14:42:57 -0600

    Use readme.md for long description in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+#read()
 
 
 setup(name='upkg',
       version='0.1.4',
       description="Package Yourself",
       url='https://github.com/jeffbuttars/upkg',
-      long_description=(open('README.md').read()),
+      long_description=read('README'),
       author="Jeff Buttars",
       author_email="jeff@jeffbuttars.com",
       packages=find_packages(exclude=['tests*']),
